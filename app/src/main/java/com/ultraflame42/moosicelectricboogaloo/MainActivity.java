e24567503b5f0d326322d327ad0670a39dde957c
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-07-03 00:55:25 +0800

    Added fragments. Tabs work

--- app/src/main/java/com/ultraflame42/moosicelectricboogaloo/MainActivity.java
+++ app/src/main/java/com/ultraflame42/moosicelectricboogaloo/MainActivity.java
@@ -3,25 +3,33 @@ package com.ultraflame42.moosicelectricboogaloo;
 import android.os.Bundle;
 import android.view.Window;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
-import com.ultraflame42.moosicelectricboogaloo.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.NavFragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.NavMenu);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
+
 
 }
